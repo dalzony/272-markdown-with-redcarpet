388054c271cafcc92f279172342a6b7e0a788f7e
Joony <joony@kakao.com>
2013-11-19 11:37:31 +0900

    file 저장경로를 rails 의 root 로변경

--- blog-after/app/models/article.rb
+++ blog-after/app/models/article.rb
@@ -3,9 +3,10 @@ class Article < ActiveRecord::Base
 
   after_save :make_file
   def make_file
-    f = File.new("./mkd/#{self.name}","w")
+    f = File.new("#{RAILS_ROOT}/mkd/#{self.name}","w")
 
     f.write(self.content.to_s)
     f.close
   end
+
 end
