4f401f63e97d66b6de25fbd5942cba03020852aa
Joony <joony@kakao.com>
2013-11-19 10:50:31 +0900

    업데이트 때 마다 파일(마크다운) 생성

--- blog-after/app/models/article.rb
+++ blog-after/app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ActiveRecord::Base
   attr_accessible :name, :content
+
+  after_save :make_file
+  def make_file
+    f = File.new("./mkd/#{self.name}","w")
+
+    f.write(self.content.to_s)
+    f.close
+  end
 end
