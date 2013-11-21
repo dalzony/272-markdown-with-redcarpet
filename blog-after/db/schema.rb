d71f93d567791ad744498565999ff83bf7ffddda
Joony <joony@kakao.com>
2013-11-21 14:49:04 +0900

    db 업데이트

--- blog-after/db/schema.rb
+++ blog-after/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110627034728) do
+ActiveRecord::Schema.define(:version => 20131119084323) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
